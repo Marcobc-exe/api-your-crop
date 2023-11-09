011ba3913085c5148391efb3d6d6b680cb428e05
Marcobc-exe <marco.developing@gmail.com>
2023-11-08 23:30:52 -0300

    Building 'Your Crop' API, backend

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000; //  chose port from here like 8080, 3001
+
+const app = express();
+
+app.use(express.json());
+
+app.use((res) => {
+  res.status(404).json({
+    message: 'Not found',
+    status: 404
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Your Crop API is listening on ${PORT}!`);
+});
