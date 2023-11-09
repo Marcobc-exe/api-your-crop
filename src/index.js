7e5574f29ad318ed6f5b7bfa423bcb1dcb53c964
Marcobc-exe <marco.developing@gmail.com>
2023-11-08 23:34:56 -0300

    New branch created, development

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ app.use(express.json());
 
 app.use((res) => {
   res.status(404).json({
-    message: 'Not found',
-    status: 404
-  })
+    message: "Not found",
+    status: 404,
+  });
 });
+
+console.log("new branch created, development!")
 
 app.listen(PORT, () => {
   console.log(`Your Crop API is listening on ${PORT}!`);
